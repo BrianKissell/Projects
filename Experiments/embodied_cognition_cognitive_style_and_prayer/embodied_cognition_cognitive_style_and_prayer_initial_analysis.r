a8b82fc94228f47c2934e3861e5d55775fbc4cba
BrianKissell <57643247+BrianKissell@users.noreply.github.com>
2021-01-31 13:43:09 -0500

    Update embodied_cognition_cognitive_style_and_prayer_initial_analysis.r
    Update the code for the embodied cognition study

--- Experiments/embodied_cognition_cognitive_style_and_prayer/embodied_cognition_cognitive_style_and_prayer_initial_analysis.r
+++ Experiments/embodied_cognition_cognitive_style_and_prayer/embodied_cognition_cognitive_style_and_prayer_initial_analysis.r
@@ -9,6 +9,10 @@ library(broom)
 library(emmeans)
 library(caret)
 library(forcats)
+library(tidyr)
+library(outliers)
+library(ggpubr)
+library(moments)
 theme_set(theme_gdocs())
 
 # Import data
@@ -32,29 +36,29 @@ glimpse(df_clean)
 # Check for missing data
 any_na(df_clean)
 vis_miss(df_clean)
-df_clean %>% filter(are_na(age) == TRUE)
-df_clean %>% filter(are_na(religious_denomination) == TRUE)
 
-# Prepare data to test assumptions
-kneel_pray <- df_clean %>% filter(physical_position =="Kneel", cognitive_task == "Pray")
-kneel_think <- df_clean %>% filter(physical_position =="Kneel", cognitive_task == "Think")
-stand_pray <- df_clean %>% filter(physical_position =="Stand", cognitive_task == "Pray")
-stand_think <- df_clean %>% filter(physical_position =="Stand", cognitive_task == "Think")
+# Impute missing data
+df_clean <- df_clean %>%
+  mutate(age  = ifelse(is.na(age), mean(age, na.rm = TRUE), age),
+         religious_denomination  = ifelse(is.na(religious_denomination), "NA", religious_denomination))
+
+#Check again for missing data
+any_na(df_clean)
 
 ############################# Need for Cognition ###########################################
 ## Check assumptions for need for cognition
 # Visually inspect distributions
 df_clean %>%
-  ggplot(aes(nfc_total, color = cognitive_task)) +
-  geom_histogram() +
-  facet_wrap(~physical_position* cognitive_task) +
+  ggplot(aes(nfc_total)) +
+  geom_density() +
+  stat_overlay_normal_density(color = "red", linetype = "dashed") +
   labs(x = "Need for Cognition")
 
-# Check for normality
-shapiro.test(stand_think$nfc_total)
-shapiro.test(kneel_pray$nfc_total)
-shapiro.test(kneel_think$nfc_total)
-shapiro.test(stand_pray$nfc_total)
+# Run Shapiro's test
+shapiro.test(df_clean$nfc_total)
+
+# Check Skewness
+skewness(df_clean$nfc_total, na.rm = TRUE)
 
 # Check for equal variances.
 bartlett.test(df_clean$nfc_total ~ df_clean$physical_position )
@@ -67,46 +71,72 @@ ggplot(df_clean, aes(x = cognitive_task, y = nfc_total, fill = physical_position
   scale_fill_manual(values=c("purple", "green"))+
   labs(title = "Cognitive Task, Physical Position, Need for Cognition", y = "Need for Cognition Score", x = "Cognitive Task", fill = "Physical Position")
 
+# Due to unequal variance across groups, the assumptions of analysis of variance are not met. Thus I instead ran Welch's Anova.
+
+df_clean %>% rstatix::welch_anova_test(nfc_total ~ cognitive_task)
+df_clean %>% rstatix::welch_anova_test(nfc_total ~ physical_position)
+df_clean %>% rstatix::group_by(physical_position) %>% welch_anova_test(nfc_total ~ cognitive_task)
+df_clean %>% rstatix::group_by(cognitive_task) %>% welch_anova_test(nfc_total ~ physical_position )
+
 ################################# Faith in Intuition ######################################
 ## Check assumptions for faith in intuition
 # Visually inspect distributions
 df_clean %>%
-  ggplot(aes(fii_total, color = cognitive_task)) +
-  geom_histogram() +
-  facet_wrap(~physical_position* cognitive_task) +
+  ggplot(aes(fii_total)) +
+  geom_density() +
+  stat_overlay_normal_density(color = "red", linetype = "dashed") +
   labs(x = "Faith in Intuition")
 
-# Check for normality
-shapiro.test(stand_think$fii_total)
-shapiro.test(kneel_pray$fii_total)
-shapiro.test(kneel_think$fii_total)
-shapiro.test(stand_pray$fii_total)
+# Run Shapiro's test
+shapiro.test(df_clean$fii_total)
 
-# Check for equal variances.
-bartlett.test(df_clean$fii_total ~ df_clean$physical_position )
-bartlett.test(df_clean$fii_total ~ df_clean$cognitive_task)
+# Check Skewness
+skewness(df_clean$fii_total, na.rm = TRUE)
+
+df_clean$fii_total_square <- df_clean$fii_total ** 2
+
+# Visually inspect distributions
+df_clean %>%
+  ggplot(aes(fii_total_square)) +
+  geom_density() +
+  stat_overlay_normal_density(color = "red", linetype = "dashed") +
+  labs(x = "Faith in Intuition Squared")
+
+# Run Shapiro's test
+shapiro.test(df_clean$fii_total_square)
+
+# Check Skewness
+skewness(df_clean$fii_total_square, na.rm = TRUE)
+
+# Check for equal variances
+bartlett.test(df_clean$fii_total_square ~ df_clean$physical_position )
+bartlett.test(df_clean$fii_total_square ~ df_clean$cognitive_task)
 
 # Create visualization of relationship between position and task
-ggplot(df_clean, aes(x = cognitive_task, y = fii_total, fill = physical_position)) +
+ggplot(df_clean, aes(x = cognitive_task, y = fii_total_square, fill = physical_position)) +
   geom_violin(trim = FALSE, position = position_dodge(1), alpha=.5)+
   geom_boxplot(position=position_dodge(1), width = .25 ) +
   scale_fill_manual(values=c("purple", "green"))+
   labs(title = "Cognitive Task, Physical Position, and Faith in Intuition", y = "Faith in Intuition", x = "Cognitive Task", fill = "Physical Position")
 
+# Run analysis of variance
+summary(aov(fii_total_square ~ cognitive_task * physical_position, data = df_clean))
+
 #################################### Cognitive Reflection Task #####################################
 ## Check assumptions for cognitive reflection test
 # Visually inspect distributions
 df_clean %>%
-  ggplot(aes(crt_total, color = cognitive_task)) +
-  geom_histogram() +
-  facet_wrap(~physical_position* cognitive_task) +
+  ggplot(aes(crt_total)) +
+  geom_density() +
+  stat_overlay_normal_density(color = "red", linetype = "dashed") +
   labs(x = "Cognitive Reflection Task")
 
 # Check for normality
-shapiro.test(stand_think$crt_total)
-shapiro.test(kneel_pray$crt_total)
-shapiro.test(kneel_think$crt_total)
-shapiro.test(stand_pray$crt_total)
+shapiro.test(df_clean$crt_total)
+
+# Check Skewness
+skewness(df_clean$crt_total, na.rm = TRUE)
+
 
 # Check for equal variances
 bartlett.test(df_clean$crt_total ~ df_clean$physical_position )
@@ -118,3 +148,9 @@ ggplot(df_clean, aes(x = cognitive_task, y = crt_total, fill = physical_position
   geom_boxplot(position=position_dodge(1), width = .25 ) +
   scale_fill_manual(values=c("purple", "green"))+
   labs(title = "Cognitive Task, Physical Position, and the Cognitive Reflection Test", y = "Cognitive Reflection Test", x = "Cognitive Task", fill = "Physical Position")
+
+#Run Welch's ANOVA
+df_clean %>% rstatix::welch_anova_test(crt_total ~ cognitive_task)
+df_clean %>% rstatix::welch_anova_test(crt_total ~ physical_position)
+df_clean %>% rstatix::group_by(physical_position) %>% welch_anova_test(crt_total ~ cognitive_task)
+df_clean %>% rstatix::group_by(cognitive_task) %>% welch_anova_test(crt_total ~ physical_position )
