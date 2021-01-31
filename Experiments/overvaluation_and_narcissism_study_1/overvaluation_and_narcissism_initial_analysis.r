25aa937454ee43035542506db39c033b0e96737d
BrianKissell <57643247+BrianKissell@users.noreply.github.com>
2021-01-31 00:03:25 -0500

    Update overvaluation_and_narcissism_initial_analysis.r
    Normalize data in overvaluation and narcissism study.

--- Experiments/overvaluation_and_narcissism_study_1/overvaluation_and_narcissism_initial_analysis.r
+++ Experiments/overvaluation_and_narcissism_study_1/overvaluation_and_narcissism_initial_analysis.r
@@ -8,6 +8,8 @@ library(naniar)
 library(broom)
 library(emmeans)
 library(outliers)
+library(ggpubr)
+library(moments)
 theme_set(theme_gdocs())
 
 # Import data
@@ -18,122 +20,81 @@ glimpse(df)
 summary(df)
 
 # Clean and prepare data
-df_clean <- df %>%
-  rename(gender = sex)%>%
-  mutate(
-    condition = factor(condition, levels = c(1,2,3), labels = c("Control", "Cold", "Overvalue")), 
-    gender = factor(gender, levels = c(0,1), labels = c("Male", "Female")),
-    npi_total = npi_total / 40,
-    date = mdy(date),
-    time = hms(time)
-  )
+df_clean <- df %>% rename(gender = sex) %>% 
+  mutate(condition = factor(condition, levels = c(1, 2, 3), labels = c("Control", "Cold", "Overvalue")), 
+         gender = factor(gender, levels = c(0, 1), labels = c("Male", "Female")), 
+         npi_total = npi_total/40, date = mdy(date), time = hms(time))
 
 # Check for missing data
 any_na(df_clean)
 vis_miss(df_clean)
 
-# Prepare data for assumption tests
-control_npi <- df_clean %>% filter(condition == "Control")
-cold_npi <- df_clean %>% filter(condition == "Cold")
-overvalue_npi <- df_clean %>% filter(condition == "Overvalue")
-
 # Outlier Detection
-boxplot(control_npi$npi_total)
-grubbs.test(control_npi$npi_total)
-boxplot(cold_npi$npi_total)
-grubbs.test(cold_npi$npi_total)
-boxplot(overvalue_npi$npi_total)
-grubbs.test(overvalue_npi$npi_total)
+boxplot(df_clean$npi_total, xlab = "Narcissitic Personality Inventory")
+grubbs.test(df_clean$npi_total)
 
-# Check assumptions for analysis of variance 
-# 1) The responses for each factor level have a normal population distribution. 
-
-# Visually inspect distributions
-df_clean %>%
-  ggplot(aes(npi_total, color = condition)) +
-    geom_histogram() +
-    facet_wrap(~condition) +
-    labs(x = "Narcissistic Personality Inventory")
-
-# Run Shapiro's test
-shapiro.test(control_npi$npi_total)
-shapiro.test(cold_npi$npi_total)
-shapiro.test(overvalue_npi$npi_total)
-
-# 2) These distributions have the same variance.
-bartlett.test(df_clean$npi_total ~ df_clean$condition)
-
-# 3) The data are independent. Random assignment was used to select groups.
-
-# Conduct analysis of variance
-anova_1 <- aov(npi_total ~ condition, data = df_clean) %>% tidy() 
-anova_1_means <- emmeans(aov(npi_total ~ condition, data = df_clean), ~condition) %>% tidy()
-
-# Create figure to evaluate the relationship between the Narcissistic Personality Inventory, the experimental condition, and the gender of the participants.
-ggplot(df_clean, aes(x = condition, y = npi_total, fill = condition)) +
-  geom_violin(trim = FALSE, position = position_dodge(1), alpha=.5)+
-  geom_boxplot(position=position_dodge(1), width = .25 ) +
-  labs(title = "Task-feedback and the Narcissistic Personality Inventory", y = "Proportion of NPI Items Selected", x = "Task Feedback Condition", fill = "Gender of Participant") +
-  scale_y_continuous( breaks = c(0.0, .25, .5, .75, 1.0))
-
-#######################################################################################
-# Prepare to test assumptions
-control_npi_male <- df_clean %>% filter(condition =="Control", gender == "Male")
-cold_npi_male <- df_clean %>% filter(condition =="Cold", gender == "Male")
-overvalue_npi_male <- df_clean %>% filter(condition =="Overvalue", gender == "Male")
-control_npi_female <- df_clean %>% filter(condition =="Control", gender == "Female")
-cold_npi_female <- df_clean %>% filter(condition =="Cold", gender == "Female")
-overvalue_npi_female <- df_clean %>% filter(condition =="Overvalue", gender == "Female")
-
-# Outlier Detection
-boxplot(control_npi_male$npi_total)
-grubbs.test(control_npi_male$npi_total)
-boxplot(cold_npi_male $npi_total)
-grubbs.test(cold_npi_male $npi_total)
-boxplot(overvalue_npi_male$npi_total)
-grubbs.test(overvalue_npi_male$npi_total)
-boxplot(control_npi_female$npi_total)
-grubbs.test(control_npi_female$npi_total)
-boxplot(cold_npi_female$npi_total)
-grubbs.test(cold_npi_female$npi_total)
-boxplot(overvalue_npi_female$npi_total)
-grubbs.test(overvalue_npi_female$npi_total)
-
-# Check assumptions for analysis of variance for the 2X3 ANOVA
-# 1) The responses for each factor level have a normal population distribution. 
-
-# Visually inspect distributions
-df_clean %>%
-  ggplot(aes(npi_total, color = gender)) +
-  geom_histogram() +
-  facet_wrap(~condition *gender) +
+# Check assumptions for analysis of variance 1) The responses for each
+# factor level have a normal population distribution.  Visually inspect
+# distribution
+df_clean %>% ggplot(aes(npi_total)) + 
+  geom_density() + 
+  stat_overlay_normal_density(color = "red",linetype = "dashed") + 
   labs(x = "Narcissistic Personality Inventory")
 
 # Run Shapiro's test
-shapiro.test(control_npi_male$npi_total)
-shapiro.test(cold_npi_male$npi_total)
-shapiro.test(overvalue_npi_male$npi_total)
-shapiro.test(control_npi_female$npi_total)
-shapiro.test(cold_npi_female$npi_total)
-shapiro.test(overvalue_npi_female$npi_total)
+shapiro.test(df_clean$npi_total)
+
+# Check Skewness
+skewness(df_clean$npi_total, na.rm = TRUE)
+
+# As per tests, the distribution is not normal and needs to be
+# transformed
+df_clean$npi_total_sqrt <- sqrt(df_clean$npi_total)
+
+# Visually inspect transformed distribution
+df_clean %>% 
+  ggplot(aes(npi_total_sqrt)) + 
+  geom_density() + 
+  stat_overlay_normal_density(color = "red", linetype = "dashed") + 
+  labs(x = "Narcissistic Personality Inventory")
+
+# Run Shapiro's test for transformed variable, and check for skewness
+shapiro.test(df_clean$npi_total_sqrt)
+skewness(df_clean$npi_total_sqrt, na.rm = TRUE)
 
 # 2) These distributions have the same variance.
-male_npi <- df_clean %>% filter(gender == "Male")
-bartlett.test(male_npi$npi_total ~ male_npi$condition )
-female_npi <- df_clean %>% filter(gender == "Female")
-bartlett.test(female_npi$npi_total ~ female_npi$condition )
+bartlett.test(df_clean$npi_total_sqrt ~ df_clean$condition)
 
-# 3) The data are independent. Random assignment was used to select groups.
+# 3) The data are independent. Random assignment was used to select
+# groups.  Conduct analysis of variance
+anova_1 <- aov(npi_total_sqrt ~ condition, data = df_clean) %>% 
+  tidy()
+print(anova_1)
 
-# Conduct analysis of variance with gender added.
-anova_2 <- aov(npi_total ~ condition * gender, data = df_clean) %>% tidy()
-anova_2_means <- emmeans(aov(npi_total ~ condition * gender, data = df_clean), ~condition | gender) %>% tidy()
-anova_2_means_gender <- emmeans(aov(npi_total ~ condition * gender, data = df_clean), ~ gender) %>% tidy()
+# Create figure to evaluate the relationship between the Narcissistic
+# Personality Inventory, the experimental condition, and the gender of
+# the participants.
+ggplot(df_clean, aes(x = condition, y = npi_total_sqrt, fill = condition)) + 
+  geom_violin(trim = FALSE, position = position_dodge(1), alpha = 0.5) + 
+  geom_boxplot(position = position_dodge(1), width = 0.25) + 
+  labs(title = "Task-feedback and the Narcissistic Personality Inventory", 
+       y = "Square Root of the Proportion of NPI Items that were Selected", 
+       x = "Task Feedback Condition", fill = "Gender of Participant") + 
+  scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1))
 
-# Create figure to evaluate the relationship between the Narcissistic Personality Inventory, the experimental condition, and the gender of the participants.
-ggplot(df_clean, aes(x = condition, y = npi_total, fill = gender)) +
-  geom_violin(trim = FALSE, position = position_dodge(1), alpha=.5)+
-  geom_boxplot(position=position_dodge(1), width = .25 ) +
-  scale_fill_manual(values=c("blue", "red"))+
-  labs(title = "Task-feedback, Gender, and the Narcissistic Personality Inventory", y = "Proportion of NPI Items Selected", x = "Task Feedback Condition", fill = "Gender of Participant") +
-  scale_y_continuous( breaks = c(0.0, .25, .5, .75, 1.0))
+###################################################################################### Conduct analysis of variance with gender added.
+anova_2 <- aov(npi_total_sqrt ~ condition * gender, data = df_clean) %>% 
+  tidy()
+print(anova_2)
+
+# Create figure to evaluate the relationship between the Narcissistic
+# Personality Inventory, the experimental condition, and the gender of
+# the participants.
+ggplot(df_clean, aes(x = condition, y = npi_total, fill = gender)) + 
+  geom_violin(trim = FALSE, position = position_dodge(1), alpha = 0.5) + 
+  geom_boxplot(position = position_dodge(1), width = 0.25) + 
+  scale_fill_manual(values = c("blue", "red")) + 
+  labs(title = "Task-feedback, Gender, and the Narcissistic Personality Inventory", 
+       y = "Square Root of the Proportion of NPI Items that were Selected", 
+       x = "Task Feedback Condition", fill = "Gender of Participant") + 
+  scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1))
