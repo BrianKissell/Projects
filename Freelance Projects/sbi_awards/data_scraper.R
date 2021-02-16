c9213b302afe115d2f47e1a3092b7f6fe19294aa
BrianKissell <57643247+BrianKissell@users.noreply.github.com>
2021-02-16 12:27:08 -0500

    Update Scraper and Data Cleaning Document
    This is the most recent version of the scraper (which now has some exception handling). This also includes the most recent version of the first report.

--- Freelance Projects/sbi_awards/data_scraper.R
+++ Freelance Projects/sbi_awards/data_scraper.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(rvest)
 library(stringr)
 library(tidyr)
+library(lubridate)
 
 # Import the links from the csv file I have stored on my google drive.
 award_links <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQBHEwhc-nPaTE7Kt9LiTJTev2_-Z0Mgw3Voi2XfFyS8EnHDtmMVynT7mb_VqUNO7wEbl21hq-N0seo/pub?output=csv")
@@ -31,38 +32,83 @@ df_scraped_data <- tibble(
   abstract = c("")
 ) 
 
-# Initiate a count, just so I can see where it is in the process.
-count = 0
-
-# Create the list of links that we will iterate over.
-award_links_full <- award_links$award_links_full
-award_links_full <- sample(award_links_full, 100)
-
-# Extract the data for every award.
-for(award_link in award_links_full){
-  
-  # Add in a pause to avoid using too many resources
-  Sys.sleep(2)
-  
-  # Create html
-  award_link_html <- read_html(award_link)
-  
-  # Collect and clean up the labels, so that they can be used as variable names
-  labels <- award_link_html %>% html_nodes('.open-label') %>% html_text() %>% 
+# Create function to collect data labels from a certain css selector, and then clean them so that they can be used as variable names.
+create_scraped_labels <- function(css_selector){
+  award_link_html %>% 
+    html_nodes(css_selector) %>% 
+    html_text() %>% 
     str_replace(pattern = ":", "") %>%
     str_replace_all(pattern = '[(]', "") %>%
     str_replace_all(pattern = '[)]', "") %>%
     str_replace_all(pattern = " ", "_") %>%
     tolower()
+}
+
+# Create function to collect the values from a css_selector that will go with each scraped label.
+create_scraped_values <- function(css_selector){
+  award_link_html %>% 
+    html_nodes(css_selector) %>% 
+    html_text()
+}
+
+# Create a function that takes a css_selector for scraped labels and a css_selector for scraped values. Then combine the labels and values in a data frame.
+create_labels_values_dataframe <- function(labels_css_selector, values_css_selector){
+  
+  # Collect and clean up the labels, so that they can be used as variable names
+  labels <- create_scraped_labels(labels_css_selector)
   
   # Collect the values that will go with each label.
-  values <- award_link_html %>% html_nodes('.open-description') %>% html_text()
+  values <- create_scraped_values(values_css_selector)
   
   # Create a dataframe, which will be used in the process.
-  labels_and_values <- tibble(labels, values)
+  labels_and_values <- tibble(labels, values) 
   
   # Turn the labels variable into the column names
-  df_data <- pivot_wider(data = labels_and_values, names_from = labels, values_from = values)
+  pivot_wider(data = labels_and_values, names_from = labels, values_from = values)
+}
+
+# Create function for reading html. It is written so that it will retry if the connection fails.
+foo <- NULL
+read_html_try <- function(x,tries) {
+  message(paste("x is",x,"remaining tries",tries))
+  withRestarts(
+    tryCatch({
+      read_html(x)
+    },
+    error=function(e) { invokeRestart("retry")}),
+    retry = function() { 
+      message("Retrying")
+      Sys.sleep(10)
+      stopifnot(tries > 0)
+      read_html_try(x,tries-1)
+    }
+  )
+}
+
+######
+# Initiate a count, just so I can see where it is in the process.
+count = 0
+
+# Create the list of links that we will iterate over.
+award_links_full <- award_links$award_links_full
+#award_links_full <- award_links$award_links_full[c(80339:85191)]
+
+
+# Extract the data for every award.
+for(award_link in award_links_full){
+  
+  # Add in a pause to avoid using too many resources
+  Sys.sleep(1)
+  
+  # Create html
+  #award_link_html <- read_html(award_link)
+  award_link_html <- read_html_try(award_link,10)
+  
+  # Collect and clean up the labels, so that they can be used as variable names
+  # Collect the values that will go with each label.
+  # Create a dataframe, which will be used in the process.
+  # Turn the labels variable into the column names
+  df_data <- create_labels_values_dataframe(labels_css_selector = '.open-label', values_css_selector = '.open-description')
   
   # Add the link to the dataframe
   df_data$link <- paste(award_link)
@@ -80,3 +126,5 @@ for(award_link in award_links_full){
   print(count)
   count = count + 1
 }
+
+write_csv(df_scraped_data, "df_scraped_data.csv")
